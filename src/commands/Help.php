b6efbb916696179bfd03fa0d498a733975b924b7
bariscodefx <barisoa029@yandex.com>
2021-04-04 20:52:31 +0300

    added a test command

---
+++ src/commands/Help.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Coded by bariscodefx with GNU License
+ */
+
+namespace bariscodefx\commands;
+
+use Discord\DiscordCommandClient;
+use Discord\CommandClient\Command;
+
+class Help extends Command {
+    
+    public function __construct(DiscordCommandClient $client) {
+        parent::__construct($client, "help", $this->callableFunction(), "Shows commands", "Shows all commands of bot", "help", 5, "You can use after 5 seconds");
+    }
+    
+    public function callableFunction($msg) {
+        $msg->channel->sendMessage("This is a test command.");
+    }
+    
+}
