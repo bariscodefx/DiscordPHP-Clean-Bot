3f86c858e039870881dc938db6912fa781fb209f
bariscodefx <76122901+bariscodefx@users.noreply.github.com>
2021-04-04 21:28:43 +0300

    update Bot.php - fixed token

--- src/Bot.php
+++ src/Bot.php
@@ -12,7 +12,7 @@ class Bot extends DiscordCommandClient {
     
     public function __construct(string $token) {
         parent::__construct([
-            'token' => 'YOUR_TOKEN'
+            'token' => $token
         ]);
     }
     
