43b1999ed2e8f67da8f7d1e6f4112cdb3fe8b2a1
bariscodefx <76122901+bariscodefx@users.noreply.github.com>
2021-04-05 10:45:46 +0300

    no tested commandloader

--- src/CommandLoader.php
+++ src/CommandLoader.php
@@ -1,1 +1,34 @@
+<?php
 
+/**
+* Coded by bariscodefx with GNU License
+*/
+
+namespace bariscodefx;
+
+use Discord\DiscordCommandClient;
+
+/**
+* CommandLoader
+*/
+class CommandLoader
+{
+
+    /**
+    * CommandLoader Constructor
+    */
+    public function __construct(DiscordCommandClient $client) {
+        $dir = __DIR__ . '/commands';
+
+        foreach (scandir($dir) as $file) {
+            $extension = substr($file, -4);
+            if ($file != '.' && $file != '..' && $extension == '.php') {
+                $class = substr($file, 0, -4);
+                $classnamespace = 'bariscodefx\commands\\' . $class;
+                $cmd = new $classnamespace;
+                $client->registerCommand($cmd);
+            }
+        }
+    }
+
+}
