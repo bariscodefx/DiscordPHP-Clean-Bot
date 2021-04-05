5fcb9eeeeadfe4e325965840c18077785a5d6fdd
bariscodefx <76122901+bariscodefx@users.noreply.github.com>
2021-04-05 15:43:02 +0300

    added say command for example

--- src/commands/Say.php
+++ src/commands/Say.php
@@ -1,1 +1,30 @@
+<?php
 
+/**
+* Coded by bariscodefx with GNU License
+*/
+
+namespace bariscodefx\commands;
+
+use Discord\DiscordCommandClient;
+
+/**
+* Say Command Class
+*/
+class Say
+{
+
+    /**
+    * Say Command Class Constructor
+    */
+    public function __construct(DiscordCommandClient $client) {
+        $client->registerCommand('say', function($msg, $args) {
+            $keyword = implode(' ', $args);
+            $msg->channel->sendMessage($keyword);
+        }, [
+            'aliases' => [
+                'speak'
+            ]
+        ]);
+    }
+}
