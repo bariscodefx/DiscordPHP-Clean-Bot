8a9d2f6255864be18427e7283d850bc527a6053a
bariscodefx <76122901+bariscodefx@users.noreply.github.com>
2021-04-05 10:56:05 +0300

    fixed bot

--- bot.php
+++ bot.php
@@ -3,13 +3,15 @@
 include __DIR__ . '/vendor/autoload.php';
 
 use Discord\DiscordCommandClient;
+use bariscodefx\CommandLoader;
 
 $bot = new DiscordCommandClient([
     'token' => 'PUT YOUR TOKEN HERE'
 ]);
 
-$bot->on('ready', function($discord){
+$bot->on('ready', function($discord) {
     echo "Bot is ready!", PHP_EOL;
+    $commandLoader = new CommandLoader;
 });
 
 $bot->run();
